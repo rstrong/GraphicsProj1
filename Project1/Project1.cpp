c27d3f779e1b86c4b9ea24abbb8fe29a1f30f221
Rick Strong <rjstrong@gmail.com>
2009-09-25 18:41:10 -0700

    Added support for axis aligned and object aligned bounding boxes

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -12,10 +12,13 @@
 
 #define debug 1
 #define PI 3.14159265
+#define ROTATE(a,i,j,k,l) g=a[i][j];h=a[k][l];a[i][j]=g-s*(h+g*tau);\
+	a[k][l]=h+s*(g-h*tau);
 
 void start_and_finish(void);
 void car(void);
 void move_car(void);
+void jacobi(float**,int,float, float**,int*);
 typedef Imath::Vec3<float> Vec3f;
 typedef Imath::Vec2<float> Vec2f;
 
@@ -27,9 +30,28 @@ struct Mesh
 
 	std::vector<int> m_vi;
 	std::vector<int> m_ci;
+
+	// Axis Oriented BB
+	float aa_xmax;
+	float aa_xmin;
+	float aa_ymax;
+	float aa_ymin;
+	float aa_zmax;
+	float aa_zmin;
+
+	// Object Oriented BB
+	float oo_xmax;
+	float oo_xmin;
+	float oo_ymax;
+	float oo_ymin;
+	float oo_zmin;
+	float oo_zmax;
 };
 
 Mesh* load(const char*);
+void setAABB(Mesh);
+
+
 int HEIGHT = 1024;
 int WIDTH = 768;
 
@@ -46,8 +68,13 @@ int window, top, bottom;
 
 int bottom_width, bottom_height, top_width, top_height;
 
+// Options
 float x_angle, y_angle;
 float viewing_angle = 0.0;
+int dispBox = 0;
+
+
+
 GLvoid *font_style = GLUT_BITMAP_HELVETICA_12;
 
 Vec3f steer;
@@ -307,6 +334,304 @@ void generateCube(void)
 		cube.m_vi.push_back(j + 2 +1);
 		cube.m_vi.push_back(cube.m_v.size() -1);
 	}
+
+}
+
+void jacobi(float **a, int n, float d[], float **v, int *nrot)
+{
+	int j,iq,ip,i;
+	float tresh,theta,tau,t,sm,s,h,g,c,*b,*z;
+	b = new float[n+1];
+	z = new float[n+1];
+	for (ip=1;ip<=n;ip++) {
+		for (iq=1;iq<=n;iq++) v[ip][iq]=0.0;
+		v[ip][ip]=1.0;
+	}
+	for (ip=1;ip<=n;ip++) {
+		b[ip]=d[ip]=a[ip][ip];
+		z[ip]=0.0;
+	}
+	*nrot=0;
+	for (i=1;i<=50;i++) {
+		sm=0.0;
+		for (ip=1;ip<=n-1;ip++) {
+			for (iq=ip+1;iq<=n;iq++)
+				sm += fabs(a[ip][iq]);
+		}
+		if (sm == 0.0) { 
+			delete z;
+			delete b;
+			return;
+		}
+		if (i < 4)
+			tresh=0.2*sm/(n*n);
+		else
+			tresh=0.0; 
+		for (ip=1;ip<=n-1;ip++) {
+			for (iq=ip+1;iq<=n;iq++) {
+				g=100.0*fabs(a[ip][iq]);
+				if (i > 4 && (float)(fabs(d[ip])+g) == (float)fabs(d[ip])
+					&& (float)(fabs(d[iq])+g) == (float)fabs(d[iq]))
+					a[ip][iq]=0.0;
+				else if (fabs(a[ip][iq]) > tresh) {
+					h=d[iq]-d[ip];
+					if ((float)(fabs(h)+g) == (float)fabs(h))
+						t=(a[ip][iq])/h; 
+					else {
+						theta=0.5*h/(a[ip][iq]); 
+						t=1.0/(fabs(theta)+sqrt(1.0+theta*theta));
+						if (theta < 0.0) t = -t;
+					}
+					c=1.0/sqrt(1+t*t);
+					s=t*c;
+					tau=s/(1.0+c);
+					h=t*a[ip][iq];
+					z[ip] -= h;
+					z[iq] += h;
+					d[ip] -= h;
+					d[iq] += h;
+					a[ip][iq]=0.0;
+					for (j=1;j<=ip-1;j++) {
+						ROTATE(a,j,ip,j,iq)
+					}
+					for (j=ip+1;j<=iq-1;j++) { 
+						ROTATE(a,ip,j,j,iq)
+					}
+					for (j=iq+1;j<=n;j++) { 
+						ROTATE(a,ip,j,iq,j)
+					}
+					for (j=1;j<=n;j++) {
+						ROTATE(v,j,ip,j,iq)
+					}
+					++(*nrot);
+				}
+			}
+		}
+		for (ip=1;ip<=n;ip++) {
+			b[ip] += z[ip];
+			d[ip]=b[ip]; 
+			z[ip]=0.0;
+		}
+	}
+	std::cout << "Too many iterations in routine jacobi" << std::endl;
+	exit(1);
+}
+
+
+void setupOOBB(Mesh *t)
+{
+	int i = 0;
+	t->oo_xmin = 1000000;
+	t->oo_xmax = -10000000;
+	t->oo_ymin = 1000000;
+	t->oo_ymax = -10000000;
+	t->oo_zmin = 1000000;
+	t->oo_zmax = -1000000;
+	
+	// Average vertex
+	Vec3f avg;
+	avg.x = 0;
+	avg.y = 0;
+	avg.z = 0;
+
+	// covariance matrix
+	float **M = new float* [4];
+	float lambda[4];
+	float **v = new float* [4];
+	int nrot = 0;
+	M[0] = new float[4];
+	M[1] = new float[4];
+	M[2] = new float[4];
+	M[3] = new float[4];
+	v[0] = new float[4];
+	v[1] = new float[4];
+	v[2] = new float[4];
+	v[3] = new float[4];
+	for(i = 0; i < 4; i++)
+	{
+		lambda[i] = 0.0;
+		for(int j = 0; j < 4; j++)
+		{
+			M[i][j] = 0.0;
+			v[i][j] = 0.0;
+		}
+	}
+
+	// Get the average vertex
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		avg.x += t->m_v[i].x;
+		avg.y += t->m_v[i].y;
+		avg.z += t->m_v[i].z;
+	}
+	avg.x = avg.x / t->m_v.size();
+	avg.y = avg.y / t->m_v.size();
+	avg.z = avg.z / t->m_v.size();
+
+	// 1 = x; 2 = y; 3 = z;
+	// Calculate covariance matrix
+	float val = 0.0;
+	//(x,x)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].x - avg.x) * (t->m_v[i].x - avg.x));
+	}
+	val = val / t->m_v.size();
+	M[1][1] = val;
+	val = 0.0;
+
+	//(x,y)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].x - avg.x) * (t->m_v[i].y - avg.y));
+	}
+	val = val / t->m_v.size();
+	M[1][2] = val;
+	val = 0.0;
+
+	//(x,z)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].x - avg.x) * (t->m_v[i].z - avg.z));
+	}
+	val = val / t->m_v.size();
+	M[1][2] = val;
+	val = 0.0;
+
+	// (y,x)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].y - avg.y) * (t->m_v[i].x - avg.x));
+	}
+	val = val / t->m_v.size();
+	M[2][1] = val;
+	val = 0.0;
+
+	// (y,y)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].y - avg.y) * (t->m_v[i].y - avg.y));
+	}
+	val = val / t->m_v.size();
+	M[2][2] = val;
+	val = 0.0;
+
+	// (y,z)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].y - avg.y) * (t->m_v[i].y - avg.y));
+	}
+	val = val / t->m_v.size();
+	M[2][3] = val;
+	val = 0.0;
+
+	// (z,x)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].z - avg.z) * (t->m_v[i].x - avg.x));
+	}
+	val = val / t->m_v.size();
+	M[3][1] = val;
+	val = 0.0;
+
+	// (z,y)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].z - avg.z) * (t->m_v[i].y - avg.y));
+	}
+	val = val / t->m_v.size();
+	M[3][2] = val;
+	val = 0.0;
+
+	// (z,z)
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		val += ( (t->m_v[i].z - avg.z) * (t->m_v[i].z - avg.z));
+	}
+	val = val / t->m_v.size();
+	M[3][3] = val;
+	val = 0.0;
+
+	// Pass to jacobi
+	jacobi(M, 3, lambda, v, & nrot);
+
+	// get our eigen vectors
+	std::vector<Vec3f> eigen;
+	for(i = 1; i < 4; i++)
+	{
+		eigen.push_back(Vec3f(v[1][i],v[2][i],v[3][i]));
+	}
+	
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		for(int j = 0; j < eigen.size(); j++)
+		{
+			if(eigen[j].dot(t->m_v[i]) < t->oo_xmin)
+			{
+				t->oo_xmin = eigen[j].dot(t->m_v[i]);
+			}
+			if(eigen[j].dot(t->m_v[i]) < t->oo_ymin)
+			{
+				t->oo_ymin = eigen[j].dot(t->m_v[i]);
+			}
+			if(eigen[j].dot(t->m_v[i]) < t->oo_zmin)
+			{
+				t->oo_zmin = eigen[j].dot(t->m_v[i]);
+			}
+			if(eigen[j].dot(t->m_v[i]) > t->oo_xmax)
+			{
+				t->oo_xmax = eigen[j].dot(t->m_v[i]);
+			}
+			if(eigen[j].dot(t->m_v[i]) > t->oo_ymax)
+			{
+				t->oo_ymax = eigen[j].dot(t->m_v[i]);
+			}
+			if(eigen[j].dot(t->m_v[i]) > t->oo_zmax)
+			{
+				t->oo_zmax = eigen[j].dot(t->m_v[i]);
+			}
+		}
+	}
+
+
+}
+
+void setupAABB(Mesh *t)
+{
+	int i = 0;
+    t->aa_xmax = -1000000;
+	t->aa_xmin = 1000000;
+	t->aa_ymax = -1000000;
+	t->aa_ymin = 10000000;
+	t->aa_zmax = -1000000;
+	t->aa_zmin = 10000000;
+	for(i = 0; i < t->m_v.size(); i++)
+	{
+		if(t->m_v[i].x > t->aa_xmax)
+		{
+			t->aa_xmax = t->m_v[i].x;
+		}
+		if(t->m_v[i].x < t->aa_xmin)
+		{
+			t->aa_xmin = t->m_v[i].x;
+		}
+		if(t->m_v[i].y > t->aa_ymax)
+		{
+			t->aa_ymax = t->m_v[i].y;
+		}
+		if(t->m_v[i].y < t->aa_ymin)
+		{
+			t->aa_ymin = t->m_v[i].y;
+		}
+		if(t->m_v[i].z > t->aa_zmax)
+		{
+			t->aa_zmax = t->m_v[i].z;
+		}
+		if(t->m_v[i].z < t->aa_zmin)
+		{
+			t->aa_zmin = t->m_v[i].z;
+		}
+	}
 }
 
 void load_models(void)
@@ -317,18 +642,29 @@ void load_models(void)
 	tmp = load("data/elephav.obj");
 	elephant = *tmp;
 
+	setupAABB(&elephant);
+	setupOOBB(&elephant);
+
 	std::cout << "loading car..." << std::endl;
 	tmp = load("data/dirtbug.obj");
 	vehicle = *tmp;
+	setupAABB(&vehicle);
+	setupOOBB(&vehicle);
 
 	std::cout << "generating plane..." << std::endl;
 	generatePlane();
+	setupAABB(&plane);
+	setupOOBB(&plane);
 
 	std::cout << "generating cylinder..." << std::endl;
 	generateCylinder();
+	setupAABB(&cylinder);
+	setupOOBB(&cylinder);
 
 	std::cout << "generating cube..." << std::endl;
 	generateCube();
+	setupAABB(&cube);
+	setupOOBB(&cylinder);
 }
 
 int strToInt(const char* c)
@@ -504,7 +840,7 @@ void renderObject(Mesh obj)
 	std::vector<Vec3f> v = obj.m_v;
 	std::vector<int> vi = obj.m_vi;
 	std::vector<Vec3f> v_color = obj.m_color;
-
+	
 	glBegin(GL_TRIANGLES);
 	for(unsigned int i = 0; i < vi.size(); i++)
 	{
@@ -512,6 +848,85 @@ void renderObject(Mesh obj)
 		glVertex3f(v[vi[i] - 1].x, v[vi[i] - 1].y, v[vi[i] -1].z);
 	}
 	glEnd();
+
+	if(dispBox == 1)
+	{
+		glBegin(GL_LINES);
+		glLineWidth(3.0);
+		glColor3f(0.0, 0.0, 0.0);
+
+		// Bottom Square
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmin);
+
+		// top square
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmax);
+
+
+		// lines
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmin);
+		glVertex3f(obj.aa_xmin, obj.aa_ymax, obj.aa_zmax);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmin, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymin, obj.aa_zmax);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmin);
+		glVertex3f(obj.aa_xmax, obj.aa_ymax, obj.aa_zmax);
+
+		glEnd();
+	}
+	if(dispBox == 2)
+	{
+		glBegin(GL_LINES);
+		glLineWidth(3.0);
+		glColor3f(0.0, 0.0, 0.0);
+
+		// Bottom Square
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmin);
+
+		// top square
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmax);
+
+
+		// lines
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmin);
+		glVertex3f(obj.oo_xmin, obj.oo_ymax, obj.oo_zmax);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmin, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymin, obj.oo_zmax);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmin);
+		glVertex3f(obj.oo_xmax, obj.oo_ymax, obj.oo_zmax);
+
+		glEnd();
+	}	
 }
 
 void
@@ -545,6 +960,7 @@ top_display(void)
 	
 	glRotated(90, 1.0, 0.0, 0.0);
 	
+	glColor3f(0.3, 1.0, 0.0);
 	renderObject(elephant);
 	glPopMatrix();
 	
@@ -710,18 +1126,41 @@ void changeView(int option)
 	redisplay_all();
 }
 
+void boundingBox(int option)
+{
+	if(option == 0)
+	{
+		dispBox = 0;
+	}
+	if(option == 1)
+	{
+		dispBox = 1;
+	}
+	if(option == 2)
+	{
+		dispBox = 2;
+	}
+	std::cout << "Bounding box now set: " << dispBox << std::endl;
+	top_reshape(top_width, top_height);
+	redisplay_all();
+}
+
 void setupMenus(void)
 {
 	
 	sub_view = glutCreateMenu(changeView);
 	glutAddMenuEntry("Top Down", 0);
 	glutAddMenuEntry("45 Degree Down", 1);
-
+	int bb = glutCreateMenu(boundingBox);
+	glutAddMenuEntry("No Bounding Box", 0);
+	glutAddMenuEntry("AA Bounding Box", 1);
+	glutAddMenuEntry("O Bounding Box", 2);
 	// Other sub menus
 
 
 	sm = glutCreateMenu(top_menu);
 	glutAddSubMenu("View", sub_view);
+	glutAddSubMenu("Bounding Box", bb);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
